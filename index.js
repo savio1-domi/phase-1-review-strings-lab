e9e53aac4867604b0797fa8e2523f8edf93a1b87
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-13 14:47:28 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code in this ficle!
 const currentUser = 'Dominic Savio'
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!` ;
 
